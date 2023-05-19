74e4b4f8c3639311483be1389238e5bfa82dc88d
Szymon Darmofał <darmofal.szymon@gmail.com>
2023-05-19 18:44:05 +0200

    Gateway request schema validation
    Request schema validation
    Template improvements
    Sending message to SQS

--- src/gateway/app.py
+++ src/gateway/app.py
@@ -3,15 +3,24 @@ import os
 import boto3
 import ujson
 
-sqs = boto3.client("sqs")
+sqs_client = boto3.client("sqs")
 
 
 def lambda_handler(event: dict, context: object) -> dict:
     queue_url = os.environ["QUEUE_URL"]
 
-    # sqs.send_message(QueueUrl=queue_url, MessageBody=ujson.dumps(message))
+    try:
+        message = {
+            "weight": event["weight"],
+            "length": event["length"],
+            "width": event["width"],
+            "height": event["height"],
+        }
+    except KeyError:
+        return {"statusCode": 400, "body": "Invalid request"}
 
-    return {
-        "statusCode": 200,
-        "body": ujson.dumps({"message": queue_url}),
-    }
+    sqs_client.send_message(
+        QueueUrl=queue_url, MessageGroupId="default", MessageBody=ujson.dumps(message)
+    )
+
+    return {"statusCode": 202}
